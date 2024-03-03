09f057e019e35e9859a3933ea10684724187a87d
mowafy001 <wafy123445@gmail.com>
2024-03-03 09:15:21 +0200

    feat: setup react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./components/layout";
+import ErrorPage from "./pages/errror";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ frontend/src/pages/errror.tsx
@@ -0,0 +1,22 @@
+import { useRouteError } from "react-router-dom";
+import styled from "styled-components";
+
+const ErrorContainer = styled.div`
+  text-align: center;
+  padding: 2rem 7rem;
+`;
+
+export default function ErrorPage() {
+  const error: any = useRouteError();
+  console.error(error);
+
+  return (
+    <ErrorContainer id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </ErrorContainer>
+  );
+}
